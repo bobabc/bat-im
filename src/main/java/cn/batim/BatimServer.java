44db0fd67d97abe01854e95f4002e8d0981c878e
Bob <bob_abc@126.com>
2022-12-22 16:32:04 +0800

    init

---
+++ src/main/java/cn/batim/BatimServer.java
@@ -0,0 +1,12 @@
+package cn.batim;
+
+/**
+ * @author zlb
+ * @version 1.0
+ * @date 2022/12/22 16:31
+ */
+public class BatimServer {
+    public static void main(String[] args) {
+        System.out.println("Hello Github");
+    }
+}
